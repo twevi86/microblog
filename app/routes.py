c8e780b196069cf6ad05c97327444c288fa5e8ed
twevi86 <etorrestendero@gmail.com>
2025-08-10 13:43:55 +0200

    ch2 & ch3

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
-@app.route("/pitorrillo")
+@app.route("/index")
 def index():
-    return "pitorrillo!!!1111"
+    user = {"username": "Esteban"}
+    posts = [
+        {"author": {"username": "Juanito"}, "body": "estoy jugando a la play"},
+        {"author": {"username": "Germán"}, "body": "el perro y el negro"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route("/esteban")
-def decir_holo():
-    return "Holo ejej"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            "login requested for user {}, remember_me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
